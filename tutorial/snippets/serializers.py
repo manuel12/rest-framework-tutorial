11efcf1f71c357196e65eb1c165c7b3497358503
Manuel <manuelpinedacabeza@gmail.com>
2021-11-11 20:07:10 +0100

    Added UserSerializer. Added owner field to SnipperSerializer. Updated imports.

--- tutorial/snippets/serializers.py
+++ tutorial/snippets/serializers.py
@@ -1,9 +1,21 @@
 from rest_framework import serializers
-from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
+from snippets.models import Snippet
+from django.contrib.auth.models import User
+
+
+
+class UserSerializer(serializers.ModelSerializer):
+    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'snippets']
 
 
 class SnippetSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+
     class Meta:
         model = Snippet
-        fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
+        fields = ['id', 'title', 'code', 'linenos', 'language', 'style', 'owner']
 
