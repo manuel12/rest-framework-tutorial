e178a2e02ffb599ba16f7e7909c79e6b01f6b147
Manuel <manuelpinedacabeza@gmail.com>
2021-11-11 20:09:21 +0100

    Added UserList and UserDetail views. Addded perform_create to SnippetList. Added permission_classes for SnippetList and SnippetDetail. Updated imports.

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,14 +1,35 @@
-from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
+from django.contrib.auth.models import User
 from rest_framework import generics
+from rest_framework import permissions
+
+from snippets.models import Snippet
+from snippets.serializers import UserSerializer, SnippetSerializer
+from snippets.permissions import IsOwnerOrReadOnly
 
 # Create your views here.
 
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class  =UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
 class SnippetList(generics.ListCreateAPIView):
+    permission_class = [permissions.IsAuthenticatedOrReadOnly]
+
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_class = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
