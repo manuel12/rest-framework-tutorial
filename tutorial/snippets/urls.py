4781c79a981fc1152c613240abb41dd88d750768
Manuel <manuelpinedacabeza@gmail.com>
2021-11-11 11:01:04 +0100

    Updated urls with classed-based views. Removed unused imports. Step 3.

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -1,12 +1,10 @@
-from django.template.defaulttags import url
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
-from rest_framework import mixins
 
 urlpatterns = [
-    path('snippets/', views.snippet_list),
-    path('snippets/<int:pk>/', views.snippet_detail)
+    path('snippets/', views.SnippetList.as_view()),
+    path('snippets/<int:pk>/', views.SnippetDetail.as_view())
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
