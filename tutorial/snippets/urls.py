0b221736d43926d4fb7d1080455e4c33bbbc8d42
Manuel <manuelpinedacabeza@gmail.com>
2021-11-11 20:07:58 +0100

    Added UserList and UserDetail paths.

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -3,6 +3,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
 urlpatterns = [
+    path('users/', views.UserList.as_view()),
+    path('users/<int:pk>/', views.UserDetail.as_view()),
     path('snippets/', views.SnippetList.as_view()),
     path('snippets/<int:pk>/', views.SnippetDetail.as_view())
 ]
