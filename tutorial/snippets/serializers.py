7e41a51165fd2c109acada31f5d013f50923d7a6
Manuel <manuelpinedacabeza@gmail.com>
2021-11-15 18:28:35 +0100

    Updated UserSerializer.snippets with HyperlinkRelatedField, also added id field. Added SnippetSerializer.highlihgt. and added url and hightlight fields

--- tutorial/snippets/serializers.py
+++ tutorial/snippets/serializers.py
@@ -3,19 +3,23 @@ from snippets.models import Snippet
 from django.contrib.auth.models import User
 
 
-
-class UserSerializer(serializers.ModelSerializer):
-    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
-
-    class Meta:
-        model = User
-        fields = ['id', 'username', 'snippets']
-
-
-class SnippetSerializer(serializers.ModelSerializer):
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    highlight = serializers.HyperlinkedIdentityField(view_name='snippet-highlight', format='html')
 
     class Meta:
         model = Snippet
-        fields = ['id', 'title', 'code', 'linenos', 'language', 'style', 'owner']
+        fields = ['url', 'id', 'highlight', 'owner', 
+                  'title', 'code', 'linenos', 'language', 'style']
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    snippets = serializers.HyperlinkedRelatedField(many=True, view_name='snippet-detail', read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['url', 'id', 'username', 'snippets']
+
+
+
 
