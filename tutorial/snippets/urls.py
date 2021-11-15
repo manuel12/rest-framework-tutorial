69279834b0a26fd98efcc8fbe93bc42af6b347c4
Manuel <manuelpinedacabeza@gmail.com>
2021-11-15 18:56:41 +0100

    Removed unused imports. Added urlpatters through router.

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -1,28 +1,14 @@
-from django.urls import path
-from rest_framework.urlpatterns import format_suffix_patterns
-from snippets import views
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from snippets.views import SnippetViewSet, UserViewSet
 
-# API endpoints
-urlpatterns = format_suffix_patterns([
-    path('', views.api_root),
 
-    path('snippets/',
-        views.SnippetList.as_view(),
-        name='snippet-list'),
+# Create a router and register viewsets with it.
+router = DefaultRouter()
+router.register(r'snippets', SnippetViewSet)
+router.register(r'user', UserViewSet)
 
-    path('snippets/<int:pk>/',
-        views.SnippetDetail.as_view(),
-        name='snippet-detail'),
-
-    path('snippets/<int:pk>/highlight/',
-        views.SnippetHighlight.as_view(),
-        name='snippet-highlight'),
-
-    path('users/',
-        views.UserList.as_view(),
-        name='user-list'),
-
-    path('users/<int:pk>/',
-        views.UserDetail.as_view(),
-        name='user-detail')
-])
+# The APIs URLs are now determined automatically by the router.
+urlpatterns = [
+    path('', include(router.urls))
+]
